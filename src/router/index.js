12c38bfc5675c705ee85f9423335cdf7e58bdc35
1knighttale1 <1knighttale1@gmail.com>
2023-05-27 02:00:16 -0400

    previo a la creacion de componentes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Qwerty from '../views/Qwerty.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/qwerty',
+      name: 'qwerty',
+      component: Qwerty
     },
     {
       path: '/about',
---
+++ src/views/Qwerty.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+        <h1>Vista Qwerty</h1>
+    </div>
+</template>
+  
