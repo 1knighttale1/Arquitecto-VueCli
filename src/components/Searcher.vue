4be3d4c1237a5514384a852c4ae0e0ddf579e154
1knighttale1 <1knighttale1@gmail.com>
2023-05-29 15:50:02 -0400

    agregando vista de imagenes (carrusel), y pequeños ajustes a la base :v

---
+++ src/components/Searcher.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="searcher-container">
+        <h3>
+            <slot name="title"></slot>    
+        </h3>
+    </div>
+</template>
+<style scope>
+.searcher-container {
+    margin-block: .5rem;
+    margin-inline: 0rem;
+}
+</style>
